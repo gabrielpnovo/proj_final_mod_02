6e4040a6bdbcb73c5d8add5a9e51b96404199cd5
gabrielpnovo <gabrielgn_@hotmail.com>
2022-08-24 19:26:09 -0300

    atualização

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ import routes from './routes/roupasRoutes.js'
 // inicializando o express
 const app = express();
 
-// recurso utilizado para permitir que a informação que está chegando (através de uma requisição "PUT" ou "POST", por exemplo) seja interpretada de maneira correta para que seja possível armazená-lo e/ou manipulá-lo.
+// recurso utilizado para permitir que a informação que está chegando (através de uma requisição "PUT" ou "POST", por exemplo) seja interpretada de maneira correta para que seja possível armazená-lo e/ou manipulá-lo;
+// é utilizado para permitir que consigamos utilizar os "params" e "params.body" nas requisições
 app.use(express.json())
 
 // está passando para o caminho "./routes/roupasRoures.js" uma instância de app para que as rotas sejam direcionadas corretamente
