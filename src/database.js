7bb3d9c6a26e40063bacd3d79e015e89e7a923e7
Tarsogarcez <tarso.garcez@gmail.com>
2022-08-28 19:39:24 -0300

    teste commit

--- src/database.js
+++ src/database.js
@@ -4,6 +4,7 @@ const tabelaRoupa = [
     { id: 3, nome: 'Calca3', tipo: 'verao', valor: 599 },
     { id: 4, nome: 'Camiseta1', tipo: 'inverno', valor: 675 },
     { id: 5, nome: 'Camiseta2', tipo: 'inverno', valor: 599 },
+    { id: 8, nome: 'Camiseta3', tipo: 'inverno', valor: 675 },
     { id: 8, nome: 'Camiseta3', tipo: 'inverno', valor: 675 }
 ]
 
@@ -11,5 +12,6 @@ let id = 1
 if (tabelaRoupa.length != 0) {
     id = Math.max.apply(null, tabelaRoupa.map(e => e.id)) + 1
 }
-
+// teste comit
+console.log (nome)
 export {tabelaRoupa, id}
