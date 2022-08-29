a9d388e0b8a21d19f9ec7d235bce264999e92eaa
gabrielpnovo <gabrielgn_@hotmail.com>
2022-08-29 18:59:12 -0300

    atualização produtos.js

--- front-end/js/produtos.js
+++ front-end/js/produtos.js
@@ -4,8 +4,6 @@ const botaoMostraVerao = document.querySelector('.mostra-verao')
 const botaoMostraInverno = document.querySelector('.mostra-inverno')
 const botaoMostraSustentavel = document.querySelector('.mostra-sustentaveis')
 
-// const botaoCriaProduto = document.querySelector('.botao-cria-produto')
-// const botaoTesteFormulario = document.querySelector('.teste-formulario')
 
 async function pegaTodos() {
     const url = 'http://localhost:3300';
@@ -91,53 +89,6 @@ async function pegaSustentavel() {
     return itensConvertidos
 }
 
-async function criaProduto() {
-    const url = 'http://localhost:3300';
-    console.log('entrou')
-
-    const itens = await fetch(`${url}/criar`, {
-
-        method: "POST",
-                
-        body: JSON.stringify({
-            "nome": "novo produto",
-            "tipo": "tipo teste",
-            "valor": 12456,
-            "descricao": "descricao qualquer",
-            "sustentavel": "sim"
-        }),
-
-
-        // mode: "cors",
-
-        headers: {
-            "Content-type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
-            "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token"
-        }
-    })
-    console.log('itensConvertidos antes')
-    console.log(req.headers.getAll('Content-Type'));
-    const itensConvertidos = await itens.json()
-    return itensConvertidos
-    // console.log('itensConvertidos depois')
-
-    // ul.innerHTML = ''
-
-    // itensConvertidos.tabelaFiltrada.forEach( e => {
-    //     ul.innerHTML = ul.innerHTML + `
-    //     <li>
-    //         <h3 class="produto-nome">${e.nome}</h3>
-    //         <p class="produto-descricao">Todos os modelos para quem gosta de velocidade.</p>
-    //         <p class="produto-preco">R$${e.valor}</p>
-    //         <button class="botao botao-produto" type="button">Adicionar ao Carrinho</button>
-    //     </li>
-    //     `
-    // })
-    // return itensConvertidos
-}
-
 botaoMostraTodos.addEventListener('click', function () {
     pegaTodos()
 });
@@ -153,8 +104,3 @@ botaoMostraInverno.addEventListener('click', function () {
 botaoMostraSustentavel.addEventListener('click', function () {
     pegaSustentavel()
 });
-
-// botaoTesteFormulario.addEventListener('click', function () {
-//     console.log('event listener')
-//     criaProduto()
-// });
